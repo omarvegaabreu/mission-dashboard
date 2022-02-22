4a80d83c1ace0ce27f611ad20078cefe299a71e7
omarvegaabreu <omarvega@hotmail.com>
2022-02-22 16:19:44 -0500

    added subit launch, launch model

--- server/routes/controllers/launches.controllers.js
+++ server/routes/controllers/launches.controllers.js
@@ -3,7 +3,13 @@ function getLaunches(req, res) {
 }
 
 function submitLaunch(req, res) {
-  console.log("submit launch");
+  const launch = {
+    date: req.body.date,
+    missionName: req.body.missionName,
+    rocketType: req.body.rocketType,
+    destinationPlanet: req.body.destinationPlanet,
+  };
+  console.log(launch);
 }
 
 function abortLaunch(req, res) {
