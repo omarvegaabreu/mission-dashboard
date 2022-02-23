05c54bfd4c1d602ebdcf608838888d253e645e74
omarvegaabreu <omarvega@hotmail.com>
2022-02-23 16:22:00 -0500

    get,post delete lauch route completed

--- server/models/launches.model.js
+++ server/models/launches.model.js
@@ -2,18 +2,21 @@
 
 const launch = [
   {
+    id: 0,
     date: "12/24/11",
     missionName: "omar alejandro",
     rocketType: "other rocket",
     destinationPlanet: "first planet",
   },
   {
+    id: 1,
     date: "11/11/11",
     missionName: "alejandro",
     rocketType: "alejandro rocket",
     destinationPlanet: "alejandro planet",
   },
   {
+    id: 2,
     date: "22/22/2222",
     missionName: "omar alejandro",
     rocketType: "other rocket",
--- server/routes/controllers/launches.controllers.js
+++ server/routes/controllers/launches.controllers.js
@@ -1,21 +1,49 @@
-const launches = require("../../models/launches.model");
+const launchesModel = require("../../models/launches.model");
 
 function getLaunches(req, res) {
-  res.json(launches);
+  console.log(launchesModel);
+  if (!launchesModel) {
+    return res.status(400).json("There are no scheduled launches");
+  }
+  res.status(200).json(launchesModel);
 }
 
 function submitLaunch(req, res) {
-  const { date, missionName, rocketType, destinationPlanet } = req.body;
+  const { id, date, missionName, rocketType, destinationPlanet } = req.body;
 
   if (!date && !missionName && !destinationPlanet) {
-    return res.status(400).json("There was no launch submitted");
+    return res
+      .status(400)
+      .json("You did not submit a launch, please try again");
   }
 
-  launches.push({ date, missionName, rocketType, destinationPlanet });
+  launchesModel.push({
+    id: launchesModel.length,
+    date,
+    missionName,
+    rocketType,
+    destinationPlanet,
+  });
 }
 
 function abortLaunch(req, res) {
-  console.log("abort launch");
+  const launchId = Number(req.params.id);
+  const currentLaunch = launchesModel[launchId];
+  // console.log(launchesModel);
+  if (!currentLaunch || currentLaunch === undefined || currentLaunch === null) {
+    return res.status(400).json("No launch up for deletion");
+  }
+
+  if (launchesModel === []) {
+    return res.status(400).json("empty object");
+  }
+
+  launchesModel.splice(
+    launchesModel.findIndex((a) => a.id === currentLaunch.id),
+    1
+  );
+
+  res.status(200).json(launchesModel);
 }
 
 module.exports = {
