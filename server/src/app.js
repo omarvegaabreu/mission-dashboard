21f5a7d1d38a236cc82d87130f4fae3a6629cf62
omarvegaabreu <omarvega@hotmail.com>
2022-02-25 16:51:27 -0500

    added morgan logger

--- server/src/app.js
+++ server/src/app.js
@@ -3,18 +3,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+const logger = require("morgan");
 const launchesRouter = require("./routes/launchesRoutes/launches.routes");
 const planetsRouter = require("./routes/planetsRoutes/planets.routes");
 
 app.use(cors({ origin: "http://localhost:3000" }));
 
-app.use((req, res, next) => {
-  const start = Date.now();
-  next();
-  const delta = Date.now() - start;
-  // console.log(`${req.method}${req.baseUrl}${req.url} request time:${delta}ms`);
-  console.log(`${req.method}${req.url} request time:${delta}ms`);
-});
+app.use(logger("dev"));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
