7f25fe261eeced8cae0bba91db6b089b0a1c49f2
omarvegaabreu <omarvega@hotmail.com>
2022-02-25 16:04:00 -0500

    deploy build script package json

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const launchesRouter = require("./routes/launchesRoutes/launches.routes");
 const planetsRouter = require("./routes/planetsRoutes/planets.routes");
 
@@ -16,7 +17,12 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(launchesRouter);
 app.use(planetsRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
