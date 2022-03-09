d610f12ac42a9e65e25e320e548e3b1f596af99a
omarvegaabreu <omarvega@hotmail.com>
2022-03-08 19:18:50 -0500

    added test

---
+++ server/src/routes/launchesRoutes/launches.test.js
@@ -0,0 +1,12 @@
+describe("Test GET /launches", () => {
+  test("it should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launches", () => {
+  test("it should be 400 response", () => {});
+  test("it should be 400 response", () => {});
+  test("should be 200 success", () => {});
+});
