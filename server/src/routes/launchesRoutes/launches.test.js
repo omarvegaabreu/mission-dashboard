07f31549c6c56478820ce4243df55720d3e8c3e9
omarvegaabreu <omarvega@hotmail.com>
2022-03-11 11:36:27 -0500

    test get and post launch

--- server/src/routes/launchesRoutes/launches.test.js
+++ server/src/routes/launchesRoutes/launches.test.js
@@ -1,12 +1,37 @@
+const request = require("supertest");
+const app = require("../../app");
+
 describe("Test GET /launches", () => {
-  test("it should respond with 200 success", () => {
-    const response = 200;
-    expect(response).toBe(200);
+  test("it should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
   });
 });
 
 describe("Test POST /launches", () => {
-  test("it should be 400 response", () => {});
-  test("it should be 400 response", () => {});
-  test("should be 200 success", () => {});
+  test("It should be 400 response.Missing launch information.", async () => {
+    const response = await request(app).post("/launches").expect(400);
+  });
+  test("it should be 400 response, invalid date.", async () => {
+    const response = await await request(app)
+      .post("/launches")
+      .send({
+        mission: "gg aby",
+        rocket: "Explorer IS1",
+        target: "Kepler-186 f",
+        launchDate: "wrong date",
+      })
+      .expect(400);
+  });
+  test("it should respond with 201 created", async () => {
+    const response = await request(app).get("/launches").send({
+      mission: "gg aby",
+      rocket: "Explorer IS1",
+      target: "Kepler-186 f",
+      launchDate: "December 31,2011",
+    });
+    expect(201);
+  });
 });
